2c4c3b6bb8a22adc098eb7e6f245942d3e873fb9
Gugzz21 <gustavodinizz966@gmail.com>
2026-02-10 09:43:26 -0300

    Crud realizado com sucesso

--- server.js
+++ server.js
@@ -25,10 +25,10 @@ function sendJson(res, statusCode, payload) {
 }
 
 let users = [
-    { id: 1, name: "Ana" },
-    { id: 2, name: "João" },
-    { id: 3, name: "Bruno" },
-    { id: 4, name: "Maria" }
+    { id: 1, name: "Ana", idade: 12 },
+    { id: 2, name: "João", idade: 13 },
+    { id: 3, name: "Bruno", idade: 14 },
+    { id: 4, name: "Maria", idade: 15 }
 
 ]
 
@@ -40,7 +40,7 @@ const server = http.createServer(async (req, res) => {
         return sendJson(res, 200, { status: "ok", timestamp: new Date().toISOString() });
     }
 
-
+    // 1. ROTA GERAL
     else if (url === "/api/v1/users") {
         //Endpoint 2: Listar usuários
         if (method === "GET") {
@@ -51,30 +51,83 @@ const server = http.createServer(async (req, res) => {
         else if (method === "POST") {
             const body = await readJsonBody(req);
 
-            users.push({ id: users.length + 1, name: body.name });
+            users.push({ id: users.length + 1, name: body.name, idade: body.idade });
             return sendJson(res, 201, users[users.length - 1]);
         }
+    }
+    
 
-        //Endpoint 4: Atualizar Usuário 
-        else if (method === "PUT" && url + "/:id") {
-            try {
-                const body = await readJsonBody(req);
-                console.log(req)
+   
+    // 2. ROTA COM ID
+    else if (url.startsWith("/api/v1/users/")) {
+        const parts = url.split("/");
+        const id = Number(parts[parts.length - 1]);
 
-
-            } catch (error) {
-                console.log(error);
-                return sendJson(res, 500, { error: "Erro ao atualizar usuário" });
+        //Endpoint 4: Listar por id
+        if (method === "GET") { 
+            
+            const user = users.find((u) => u.id === id );
+            if (!user){ 
+            return sendJson(res, 404, {error: "Usuário não encontrado."});
             }
+            return sendJson(res, 200, user);
+            
+        }
+
+
+        //Endpoint 5: Atualizar todos os atributos de um usuário
+        else if (method === "PUT") {
+            const body = await readJsonBody(req);
+            const user = users.find((u) => u.id === id );
+            
+            if (user){ 
+                user.name = body.name; 
+                user.idade = body.idade;
+                return sendJson(res, 201, user);
+            }
+
+            else 
+                return sendJson(res, 404, {error: "Usuário não encontrado."});
+
+        }
+
+        //Endpoint 6: Atualizar um dos atributos de um usuário
+        else if (method === "PATCH") {
+            const body = await readJsonBody(req);
+            const user = users.find((u) => u.id === id );
+            
+            if (user){ 
+                user.name = body.name; 
+                user.idade = body.idade;
+                return sendJson(res, 201, user);
+            }
+
+            else 
+                return sendJson(res, 404, {error: "Usuário não encontrado."});
 
         }
 
 
+         else if (method === "DELETE"){
+            const user = users.find((u) => u.id === id );
+
+
+            if (!user){ 
+                return sendJson(res, 404, {error: "Usuário não encontrado."});
+            }
+
+            const index = users.findIndex((u) => u.id === id );
+            users.splice(index, 1);
+            res.writeHead(204);
+            res.end();
+        
+        }
     }
 
-
-
-})
+    else 
+        return sendJson(res, 404, {error: "Rota não encontrada !" });
+    
+    })
 
 
 const PORT = process.env.PORT || 3000;
